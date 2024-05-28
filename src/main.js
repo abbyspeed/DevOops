ed1ec7567e1891af4dc81e90353aa3a682229ab5
abbyspeed <wnabihah09@gmail.com>
2024-05-28 15:17:39 +0800

    updated router configuration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
 import CustomForm from "./components/CustomForm.vue"
-
-import router from "./router.js"
 
 const app = createApp(App);
 app.component("customForm", CustomForm);
